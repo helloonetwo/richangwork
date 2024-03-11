ad64aec3c4929058910692d027b6b10142ad8dcb
zhangxiaowei <xiaowei@nw.daoxinkj.com>
2024-03-11 11:33:24 +0800

    feat: 前端规范

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -57,10 +57,10 @@ module.exports = {
             'JavaScript 编码规约',
             'Node.js 编码规约',
             'TypeScript 编码规约',
-            'VS Code 配置SonarLint',
+            'Vue3编码规约',
             'Git提交信息规范',
             'Git版本管理规范',
-            'vue编码规范'
+            'VS Code 配置SonarLint'
           ],
         },
       ],
--- docs/coding/README.md
+++ docs/coding/README.md
@@ -1,1 +1,12 @@
 # 前端规范
+
+- 通用编码规约
+- HTML 编码规约
+- CSS 编码规约
+- JavaScript 编码规约
+- Node.js 编码规约
+- TypeScript 编码规约
+- Vue3编码规约
+- Git提交信息规范
+- Git版本管理规范
+- VS Code 配置SonarLint
